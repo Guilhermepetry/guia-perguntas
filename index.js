758b02634d4c10bbf28329f16ca0f55b0a623715
Guilherme Petry <guilhermepetryoficial@hotmail.com>
2020-08-13 15:49:28 -0300

    Setting template engine ejs

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Olá, mundo!");
+  res.render("index.js");
 });
 
 app.listen(8080, () => {
