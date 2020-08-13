4878d337b63d9adc936bb6011f7ced75c908e7e3
Guilherme Petry <guilhermepetryoficial@hotmail.com>
2020-08-13 17:10:35 -0300

    Finalize the question route

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
 const express = require("express");
 const app = express();
 
+//Setting
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/:nome/:lang", (req, res) => {
-  var nome = req.params.nome;
-  var lang = req.params.lang;
-  var exibirMsg = false;
-  var produtos = [
-    { nome: "Doritos", preco: 3.14 },
-    { nome: "Coca-cola", preco: 3.5 },
-    { nome: "Água", preco: 1.0 },
-    { nome: "RedBull", preco: 6.0 },
-    { nome: "Pipoca", preco: 1.5 },
-    { nome: "Nescal", preco: 4.0 },
-  ];
-  res.render("index", {
-    nome: nome,
-    lang: lang,
-    empresa: "youtube",
-    inscritos: 100,
-    msg: exibirMsg,
-    produtos: produtos,
-  });
+//Rotas
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
+app.get("/question", (req, res) => {
+  res.render("question");
+});
+
+//Start server
 app.listen(8080, () => {
   console.log("App rodando!");
 });
