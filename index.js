17ac6e268904abf32435fcdebb0e4b2fe331fa38
Guilherme Petry <guilhermepetryoficial@hotmail.com>
2020-08-13 16:23:58 -0300

    repetition structure in ejs

--- index.js
+++ index.js
@@ -6,13 +6,22 @@ app.set("view engine", "ejs");
 app.get("/:nome/:lang", (req, res) => {
   var nome = req.params.nome;
   var lang = req.params.lang;
-  var exibirMsg = true;
+  var exibirMsg = false;
+  var produtos = [
+    { nome: "Doritos", preco: 3.14 },
+    { nome: "Coca-cola", preco: 3.5 },
+    { nome: "Água", preco: 1.0 },
+    { nome: "RedBull", preco: 6.0 },
+    { nome: "Pipoca", preco: 1.5 },
+    { nome: "Nescal", preco: 4.0 },
+  ];
   res.render("index", {
     nome: nome,
     lang: lang,
     empresa: "youtube",
     inscritos: 100,
     msg: exibirMsg,
+    produtos: produtos,
   });
 });
 
