3b13984003a690410e7f1e20f9d70b3773127d81
Guilherme Petry <guilhermepetryoficial@hotmail.com>
2020-08-14 16:25:24 -0300

    parse the request body

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 //Setting
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Rotas
 app.get("/", (req, res) => {
@@ -12,6 +16,19 @@ app.get("/", (req, res) => {
 
 app.get("/question", (req, res) => {
   res.render("question");
+});
+
+app.post("/savequestion", (req, res) => {
+  var title = req.body.title;
+  var description = req.body.description;
+  res.send(
+    "Formulário recebido com sucesso! " +
+      "Título: " +
+      title +
+      ", " +
+      "Descrição: " +
+      description
+  );
 });
 
 //Start server
